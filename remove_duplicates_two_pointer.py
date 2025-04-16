10e5e6024466cfff1a8cdd8937df1282cc8974db
Ajeeth12 <ajeethaj99@gmail.com>
2025-04-16 15:32:57 +0530

    Remove duplicates

---
+++ remove_duplicates_two_pointer.py
@@ -0,0 +1,14 @@
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+
+    slow = 0  # Index to place the next unique element
+
+    for fast in range(1, len(nums)):
+        if nums[fast] != nums[slow]:
+            slow += 1
+            nums[slow] = nums[fast]
+
+    print(slow + 1)  # The number of unique elements
+nums = [1, 1, 2, 2, 3, 4, 7, 8, 8]
+remove_duplicates(nums)
